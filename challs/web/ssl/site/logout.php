98ca7eb07ff6dab4946606b9db42169065400fe6
BarbanAlberto <9698260@itisrossi.vi.it>
2025-05-30 00:03:23 +0200

    Fixati alcuni Docker e il logout di web1

--- challs/web/ssl/site/logout.php
+++ challs/web/ssl/site/logout.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $params = session_get_cookie_params();
 setcookie(session_name(), '', time() - 1, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 session_destroy();
